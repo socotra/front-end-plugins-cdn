3ef6825b2b551d884f31baea53b4843399f146fc
Devin Clark <devin.clark27@gmail.com>
2022-06-24 14:22:16 -0500

    Append examples scripts to FE landing zone

--- examples/pink-button.js
+++ examples/pink-button.js
@@ -1,8 +1,16 @@
-(function insertPinkButton() {
+(function insertBlueButton() {
+  // Use the designated element to insert the pink button
+  // This element will be removed at logout
+  var fePlugins = document.getElementById("fe-plugins");
+
+  // Create the element
   var button = document.createElement("button");
   button.innerHTML = "Pink Button";
   button.style =
     "border-radius:40px;height:200px;width:400px;background-color:pink;color:white;bottom:0;left:0;position:absolute;z-index:9999;margin:120px";
-  document.body.appendChild(button);
-  return insertPinkButton;
-})();
+
+  // Attach the element to the landing zone
+  fePlugins.appendChild(button);
+
+  return insertBlueButton;
+})(); // hello
